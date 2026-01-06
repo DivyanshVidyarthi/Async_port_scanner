cdcbe8da1e81cbba55c0605ae8a48d321ba91e89
Divyansh Vidyarthi <97382845+DivyanshVidyarthi@users.noreply.github.com>
2026-01-06 20:49:28 +0530

    Enhance asyncportscanner.py with explanatory comments
    Added comments to clarify the purpose of imports and functions.

--- asyncportscanner.py
+++ asyncportscanner.py
@@ -1,17 +1,17 @@
-import asyncio
-import time 
+import asyncio #This function is imported to read all the ports asynchronously as the ports are scanned actually fast without any traffic stoppage(literally!!!)
+import time #IT'S TIME MAN WHAT ELSE COULD THIS BE FOR...
 
-async def port_scanner(host,port,timeout=0.5):
-    try:
-        reader,writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout)
+async def port_scanner(host,port,timeout=0.5): #Calling the port scanning function 
+    try: #A method similar to if-else and instead of giving a conditon earlier it takes the code inside of it as a condition
+        reader,writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout) #I know I knwo reader is not used anywhere but we use it to actually know if the port is getting the call or not, we can't remove it just like that
         writer.close()
         await writer.wait_closed()
         return port
     except Exception:
        return None
-    
+    #I don't think I need to tell about the rest
 
-async def main(host, port_start=1, port_end=8000, concurrency = 100):
+async def main(host, port_start=1, port_end=8000, concurrency = 100): # function where scanning actually happens...
     start_time = time.perf_counter()
     port = range(port_start, port_end+1)
     sem = asyncio.Semaphore(concurrency)
