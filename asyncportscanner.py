1e8ee23e1777dc44861e4fbf8a608d13773f1035
Divyansh Vidyarthi <97382845+DivyanshVidyarthi@users.noreply.github.com>
2026-01-14 10:55:53 +0530

    Refactor comments and improve code readability

--- asyncportscanner.py
+++ asyncportscanner.py
@@ -3,7 +3,7 @@ import time #IT'S TIME MAN WHAT ELSE COULD THIS BE FOR...
 
 async def port_scanner(host,port,timeout=0.5): #Calling the port scanning function 
     try: #A method similar to if-else and instead of giving a conditon earlier it takes the code inside of it as a condition
-        reader,writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout) #I know I knwo reader is not used anywhere but we use it to actually know if the port is getting the call or not, we can't remove it just like that
+        reader,writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout) #I know I know reader is not used anywhere but we use it to actually know if the port is getting the call or not, we can't remove it just like that
         writer.close()
         await writer.wait_closed()
         return port
@@ -11,11 +11,11 @@ async def port_scanner(host,port,timeout=0.5): #Calling the port scanning functi
        return None
     #I don't think I need to tell about the rest
 
-async def main(host, port_start=1, port_end=8000, concurrency = 100): # function where scanning actually happens...
+async def main(host, port_start=1, port_end=8000, concurrency = 100): # function where scanning actually happens
     start_time = time.perf_counter()
     port = range(port_start, port_end+1)
     sem = asyncio.Semaphore(concurrency)
-    async def sem_scan(p):
+    async def sem_scan(p): #A Semaphore: What it does is limiting the current resources in use by leaving the task if it takes to much time or power. 
         async with sem:
             return await port_scanner(host, p)
     task  = [asyncio.create_task(sem_scan(p)) for p in port]
